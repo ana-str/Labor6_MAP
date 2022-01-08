fdfd63a53dce94075a1b984fb6c146398503ea92
ana-str <anastrugar20@gmail.com>
2022-01-08 15:06:47 +0200

    view

--- src/main/java/view/StudentView.java
+++ src/main/java/view/StudentView.java
@@ -1,6 +1,114 @@
 package view;
 
+import controller.CourseController;
+import controller.RegistrationSystem;
+import controller.StudentController;
+import controller.TeacherController;
+import entities.Student;
+import repository.CourseJdbcRepository;
+import repository.StudentJdbcRepository;
+import repository.TeacherJdbcRepository;
+import exceptions.NonExistingDataException;
+import javafx.fxml.FXML;
 
-import controller.CourseControllerTest;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+
 public class StudentView {
+    private static Student student;
+
+    public RegistrationSystem initializeRegistrationSystem() {
+        CourseJdbcRepository courseRepository = new CourseJdbcRepository();
+        CourseController courseController = new CourseController(courseRepository);
+
+        StudentJdbcRepository studentRepository = new StudentJdbcRepository();
+        StudentController studentController = new StudentController(studentRepository);
+
+        TeacherJdbcRepository teacherRepository = new TeacherJdbcRepository();
+        TeacherController teacherController = new TeacherController(teacherRepository);
+
+        return new RegistrationSystem(studentController, courseController, teacherController);
+    }
+
+    @FXML
+    Button totalCredits;
+
+    @FXML
+    Label message;
+
+    @FXML
+    Label message2;
+
+    @FXML
+    Label notFoundText;
+
+    @FXML
+    Label credits;
+
+    @FXML
+    Button studentLogIn;
+
+    @FXML
+    TextField idStudent;
+
+    @FXML
+    Label course;
+
+    @FXML
+    TextField courseName;
+
+    @FXML
+    Button register;
+
+    /**
+     * log in for students
+     * @throws IOException if an I/O exception has occurred
+     */
+    public void loginStudent() throws IOException {
+        int id = Integer.parseInt(idStudent.getText());
+        RegistrationSystem registrationSystem = initializeRegistrationSystem();
+        try {
+            student = registrationSystem.getStudentController().findByID(id);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        if (student == null) {
+            notFoundText.setText("Student not found");
+            return;
+        }
+
+        HelloController main = new HelloController();
+        main.changeSceneStudent("student-after-login.fxml");
+    }
+
+    /**
+     * shows the total number of credits for the logged in student
+     */
+    public void showTotalCredits() {
+        credits.setText(String.format("You have %s credits", student.getTotalCredits()));
+    }
+
+    /**
+     * registers the logged in student to a course
+     */
+    public void register() {
+        String courseTitle = courseName.getText();
+        RegistrationSystem registrationSystem = initializeRegistrationSystem();
+        boolean registered = false;
+        try {
+            registered = registrationSystem.register(courseTitle, student.getStudentID());
+        } catch (SQLException | NonExistingDataException e) {
+            e.printStackTrace();
+        }
+        if (registered) {
+            message2.setText("Student registered");
+        }
+        else
+            message2.setText("Registration failed!");
+    }
 }
--- src/main/java/view/TeacherView.java
+++ src/main/java/view/TeacherView.java
@@ -1,4 +1,128 @@
 package view;
 
+import controller.CourseController;
+import controller.RegistrationSystem;
+import controller.StudentController;
+import controller.TeacherController;
+import entities.Course;
+import entities.Student;
+import entities.Teacher;
+import repository.CourseJdbcRepository;
+import repository.StudentJdbcRepository;
+import repository.TeacherJdbcRepository;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
 public class TeacherView {
+    private static Teacher teacher;
+
+    public RegistrationSystem initializeRegistrationSystem() {
+        CourseJdbcRepository courseRepository = new CourseJdbcRepository();
+        CourseController courseController = new CourseController(courseRepository);
+
+        StudentJdbcRepository studentRepository = new StudentJdbcRepository();
+        StudentController studentController = new StudentController(studentRepository);
+
+        TeacherJdbcRepository teacherRepository = new TeacherJdbcRepository();
+        TeacherController teacherController = new TeacherController(teacherRepository);
+
+        return new RegistrationSystem(studentController, courseController, teacherController);
+    }
+
+
+    @FXML
+    Label message;
+
+    @FXML
+    Label message2;
+
+    @FXML
+    Label notFoundText;
+
+    @FXML
+    Button teacherLogIn;
+
+    @FXML
+    TextField idTeacher;
+
+    @FXML
+    Button refresh;
+
+    @FXML
+    Button showStudents;
+
+    @FXML
+    Label course;
+
+    @FXML
+    ListView<Student> listView;
+
+    @FXML
+    TextField courseName;
+
+    /**
+     * log in for teachers
+     * @throws IOException if an I/O exception has occurred
+     */
+    public void loginTeacher() throws IOException {
+        int id = Integer.parseInt(idTeacher.getText());
+        RegistrationSystem registrationSystem = initializeRegistrationSystem();
+        try {
+            teacher = registrationSystem.getTeacherController().findByID(id);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        if (teacher == null) {
+            notFoundText.setText("Teacher not found");
+            return;
+        }
+
+        HelloController main = new HelloController();
+        main.changeSceneTeacher("teacher-after-login.fxml");
+    }
+
+    /**
+     * shows all students enrolled to a certain course of the logged in teacher
+     */
+    public void showEnrolledStudents() {
+        message2.setVisible(false);
+        String courseTitle = courseName.getText();
+        RegistrationSystem registrationSystem = initializeRegistrationSystem();
+        Course foundCourse = teacher.getCourses().stream()
+                .filter(myCourse -> myCourse.getName().equals(courseTitle))
+                .findFirst()
+                .orElse(null);
+
+        if (foundCourse == null) {
+            message2.setText("This is not your course!");
+            message2.setVisible(true);
+            return;
+        }
+
+        try {
+            List<Student> students = registrationSystem.retrieveStudentsEnrolledForACourse(courseTitle);
+            ObservableList<Student> observableList = FXCollections.observableList(students);
+            listView.setItems(observableList);
+            refresh.setVisible(true);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * refreshes page to show the updated list of enrolled students
+     */
+    public void refreshPage() {
+        showEnrolledStudents();
+        message2.setText("Page was refreshed");
+        message2.setVisible(true);
+    }
 }
